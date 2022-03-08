661d7b8fb1540642c8e3e9bc12d9eb13eb0d6b8e
Sara894 <t48250klo@gmail.com>
2022-03-08 14:37:48 +0300

    обычный код без LINQ

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -7,25 +7,24 @@ namespace LINQ
     {
         static void Main(string[] args)
         {
-            string[] people = { "Tom", "Tailer","Bob","Sam","Tim","Tomas","Tomsin" };
-
-            //создаем новый список для результатов
+            //источник данных - любой
+            string[] people = { "Tom", "Tim", "Masha", "Mitya" };
+            //массив для результата запроса
             var selectedPeople = new List<string>();
 
             //проходим по массиву 
-            foreach(string person in people )
+            foreach(string person in people)
             {
-                //если строка начинается на букву T
+                //если строка начинается на букву Т забираем слово
                 if (person.ToUpper().StartsWith("T"))
                     selectedPeople.Add(person);
             }
 
-            //сортируем список
+            //сортируем список выбранных имен
             selectedPeople.Sort();
 
             foreach (string person in selectedPeople)
                 Console.WriteLine(person);
-
         }
     }
 }
