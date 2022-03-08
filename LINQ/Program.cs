72a0fc5c78abfe6cabad80f52b5de8f42e1a7b46
Sara894 <t48250klo@gmail.com>
2022-03-08 14:28:22 +0300

    Добавьте файлы проекта.

---
+++ LINQ/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] people = { "Tom", "Tailer","Bob","Sam","Tim" };
+
+            //создаем новый список для результатов
+            var selectedPeople = new List<string>();
+
+            //проходим по массиву 
+            foreach(string person in people )
+            {
+                //если строка начинается на букву T
+                if (person.ToUpper().StartsWith("T"))
+                    selectedPeople.Add(person);
+            }
+
+            //сортируем список
+            selectedPeople.Sort();
+
+            foreach (string person in selectedPeople)
+                Console.WriteLine(person);
+
+        }
+    }
+}
